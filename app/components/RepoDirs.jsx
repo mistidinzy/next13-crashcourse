eabcd6351ed2b5b520c467d203ab7369777ac328
Misti <82716950+mistidinzy@users.noreply.github.com>
2023-05-15 14:20:44 -0500

    create RepoDirs component

---
+++ app/components/RepoDirs.jsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+
+async function fetchRepoContents(name) {
+  const response = await fetch(
+    'https://api.github.com/repos/mistidinzy/${name}/contents',
+    { next: { revalidate: 60 * 60 * 24 } }
+  );
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const contents = await response.json();
+  return contents;
+}
+
+const RepoDirs = async ({ name }) => {
+  const contents = await fetchRepoContents(name);
+  const values = Object.values(contents);
+  const dirs = values.filter((content) => content.type === 'dir');
+
+  return (
+    <div>
+      <h3>Directories</h3>
+      <ul>
+        {dirs.map((dir) => (
+          <li key={dir.path}>
+            <Link href={`/code/repos/${name}/${dir.path}`}></Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RepoDirs;
