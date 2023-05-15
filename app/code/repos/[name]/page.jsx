542ad6ba6d5b6fc5149cc31a733f0355a3bc1440
Misti <82716950+mistidinzy@users.noreply.github.com>
2023-05-15 14:22:34 -0500

    Adds suspense and repodirs to repos page

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,5 +1,7 @@
+import { Suspense } from 'react';
 import Link from 'next/link';
 import Repo from '@/app/components/Repo';
+import RepoDirs from '@/app/components/RepoDirs';
 
 const RepoPage = ({ params: { name } }) => {
   return (
@@ -9,7 +11,12 @@ const RepoPage = ({ params: { name } }) => {
       <Link href='/code/repos' className='btn btn-back'>
         Back to Repositories
       </Link>
-      <Repo name={name} />
+      <Suspense fallback={<div>Loading repo...</div>}>
+        <Repo name={name} />
+      </Suspense>
+      <Suspense fallback={<div>Loading directories...</div>}>
+        <RepoDirs name={name} />
+      </Suspense>
     </div>
   );
 };
