4ade25cdb0151aaf95657d58faafe701e8bbd098
Misti <82716950+mistidinzy@users.noreply.github.com>
2023-05-15 20:32:15 -0500

    removes unused home links

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,10 @@
 import React from 'react';
-import Link from 'next/link';
+// import Link from 'next/link';
 
 const HomePage = () => {
   return (
     <div>
       <h1>Welcome</h1>
-      <ul>
-        <li>
-          <Link href='/'>Home</Link>
-        </li>
-        <li>
-          <Link href='/'>Home</Link>
-        </li>
-        <li>
-          <Link href='/'>Home</Link>
-        </li>
-      </ul>
     </div>
   );
 };
