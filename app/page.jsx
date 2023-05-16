51895fac4396b39f9ac1ca50701383518d8bd4de
Misti <82716950+mistidinzy@users.noreply.github.com>
2023-05-15 21:17:03 -0500

    Create Courses component

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
 // import Link from 'next/link';
+import Courses from './components/Courses';
 
 const HomePage = () => {
   return (
-    <div>
+    <>
       <h1>Welcome</h1>
-    </div>
+      <Courses />
+    </>
   );
 };
 
